6174f66ed80bcaf8f27a14bf20cf16a6ed38c9ef
regnierd <javivi_lp_96@hotmail.es>
2021-05-07 17:18:41 +0100

    Definicion de las clases de api.movimientos

--- ProyectoBait/bait/src/main/java/es/iespuertodelacruz/bait/api/movimientos/DetalleVenta.java
+++ ProyectoBait/bait/src/main/java/es/iespuertodelacruz/bait/api/movimientos/DetalleVenta.java
@@ -1,5 +1,39 @@
 package es.iespuertodelacruz.bait.api.movimientos;
 
-public class DetalleVenta {
+public class DetalleVenta extends Factura{
+    private float descuentoUnidad;
     
+    /**
+     * Constructor por defecto vacio
+     */
+    public DetalleVenta() {
+    }
+
+    /**
+     * Constructor con todos los atributos
+     * @param unidades de la detalle venta
+     * @param costoUnidad de la detalle venta
+     * @param total de la detalle venta
+     * @param descuentoUnidad de la detalle venta
+     */
+    public DetalleVenta(int unidades, float costoUnidad, float total, float descuentoUnidad) {
+        super(unidades, costoUnidad, total);
+        this.descuentoUnidad = descuentoUnidad;
+    }
+
+    //Getters y Setters
+    public float getDescuentoUnidad() {
+        return this.descuentoUnidad;
+    }
+
+    public void setDescuentoUnidad(float descuentoUnidad) {
+        this.descuentoUnidad = descuentoUnidad;
+    }
+
+
+    @Override
+    public String toString() {
+        return getDescuentoUnidad() + "";
+    } 
+
 }
